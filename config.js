6c377f41b87c5475ab450214d8d92e914ab6507f
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-05-14 15:15:53 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,23 @@
-var api = "http://sg1.npool.pw:3015";
+var api = "http://sg1.npool.pw:3010";
 
 var poolHost = "sg1.npool.pw";
 
-var blockExplorer = "http://blockexplorer.pw/trit";
+var blockExplorer = "http://explorer.tritonproject.org/";
 
 var cryptonatorWidget = ["BTC-USD", "TRIT-BTC"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchain_block";
+var blockchainExplorer = "http://explorer.tritonproject.org/?hash={id}#blockchain_block";
 
-var transactionExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchain_transaction";
+var transactionExplorer = "http://explorer.tritonproject.org/?hash={id}#blockchain_transaction";
 
 var themeCss = "themes/default-theme.css";
 
 var networkStat = [
-        ["npool.pw/tritonv2", "http://sg1.npool.pw:3015"]
+        ["npool.pw/triton", "http://sg1.npool.pw:3010"],
+        ["triton.fatpanda.club", "http://54.148.101.99:8018"],
+        ["luckypool.io", "https://trit.luckypool.io/api/pool"],
+        ["trit.newpool.pw", "http://trit.newpool.pw:8120"],
+        ["cryptoknight.cc/triton","https://cryptoknight.cc/rpc/triton"]
 ];
