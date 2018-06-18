db623aedb0a4bd047059ef149f3918e5f090d733
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-06-18 14:22:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var api = "https://api.sg1.npool.pw/trit";
+var api = "https://apisg1.npool.pw/trit";
 
 var poolHost = "sg1.npool.pw";
 
@@ -13,7 +13,7 @@ var transactionExplorer = "http://explorer.tritonproject.org/?hash={id}#blockcha
 var themeCss = "themes/default-theme.css";
 
 var networkStat = [
-        ["npool.pw/triton", "https://api.sg1.npool.pw/trit"],
+        ["npool.pw/triton", "https://apisg1.npool.pw/trit"],
         ["triton.fatpanda.club", "http://54.148.101.99:8018"],
         ["luckypool.io", "https://trit.luckypool.io/api/pool"],
         ["trit.newpool.pw", "http://trit.newpool.pw:8120"],
